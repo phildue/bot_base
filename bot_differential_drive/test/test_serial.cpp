dfc17f3f61aa5bf00a292cf728fd026f6577856f
phild <phild@protonmail.com>
2024-02-11 05:38:57 +0000

    cleanup

--- bot_differential_drive/test/test_serial.cpp
+++ bot_differential_drive/test/test_serial.cpp
@@ -12,54 +12,34 @@ using namespace mn::CppLinuxSerial;
 #include "SerialProtocol.h"
 
 int main() {
-  SerialProtocol serial;
-  auto port = std::make_shared<SerialPort>();
-  // Create serial port object and open serial port
-  port->SetBaudRate(BaudRate::B_9600);
-  port->SetDevice("/dev/ttyACM0");
-  // SerialPort serialPort("/dev/ttyACM0", 13000);
+  auto port = std::make_shared<SerialPort>("/dev/ttyACM0", BaudRate::B_9600);
   port->SetTimeout(100); // Block when reading until any data is received
   port->Open();
 
-  // Write some ASCII datae
-  const float T_MS = 10;
+  const int T_MS = 100;
 
   for (int i = 0; i < 1000; i++) {
     float setPoint = std::sin((float)i / 10.0) * 12.0;
     if (setPoint < 4) {
       setPoint = 4;
     }
-    //        setPoint = 0;
-    auto msgOut =
-        std::make_shared<SerialProtocol::MsgCmdVel>(setPoint, -1 * setPoint, i);
-    //        auto msgOut =
-    //        std::make_shared<SerialProtocol::MsgCmdVel>(0,-1*setPoint,i); auto
-    //        msgOut =
-    //        std::make_shared<SerialProtocol::MsgCmdVel>(setPoint,0,i);
-    std::cout << "Sending:\n" << msgOut->str() << std::endl;
+    serial_protocol::MsgCmdVel setPointMsg{setPoint, -1 * setPoint,
+                                           (uint64_t)i};
+    std::cout << "Sending:\n" << setPointMsg.str() << std::endl;
+    port->Write(setPointMsg.serialStr());
 
-    port->Write(msgOut->serialStr());
-    try {
-      serial.parse(10, port);
+    serial_protocol::MsgQueryState queryState{(uint64_t)i};
+    std::cout << "Sending:\n" << queryState.str() << std::endl;
+    port->Write(queryState.serialStr());
 
-    } catch (const SerialProtocol::ParseError &e) {
-      std::cerr << e.what() << "\n Message: " << e._parsedMessage << std::endl;
+    auto raw = port->ReadUntil('\n', 100);
+    if (serial_protocol::parseType(raw) == serial_protocol::MsgType::STATE) {
+      serial_protocol::MsgState state{raw};
+      std::cout << "Received:\n" << state.str() << std::endl;
+    } else {
+      std::cerr << "Error! Received:\n" << raw << std::endl;
     }
-
-    // Handle newest state message
-    if (!serial._messagesState.empty()) {
-      auto msg = serial._messagesState[serial._messagesState.size() - 1];
-      std::cout << msg->str() << std::endl;
-    }
-
-    for (const auto &msg : serial._messages) {
-      std::cout << msg->str() << std::endl;
-    }
-
-    serial._messagesState.clear();
-    serial._messages.clear();
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(T_MS));
   }
 
   // Close the serial port
